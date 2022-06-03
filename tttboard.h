0c5f33494c83613155110c2f5e4a806d0aa1d390
William Lillis <wjlillis22@wabash.edu>
2022-06-03 12:13:29 -0400

    started work on CheckEndConditions function

--- tttboard.h
+++ tttboard.h
@@ -8,23 +8,35 @@
 #include<assert.h>
 #include<string>
 
-class player {
+enum PlayerSide {
+	X,
+	O,
+	Cat
+};
+
+
+class tttPlayer {
 private:
-	uint8_t whichplayer;
+	PlayerSide player;
 public:
-	player(uint8_t which)
+	// constructor, just allows specificiation of P1 (X's) or P2 (O's)
+	tttPlayer(PlayerSide which)
 	{
-		assert(which == 0 || which == 1);
-		whichplayer = which;
+		assert(which == PlayerSide::X || which == PlayerSide::O);
+		player = which;
 	}
-	uint8_t getplayer()
+	// Destructor, is this even needed
+	~tttPlayer()
 	{
-		return whichplayer;
+		;
 	}
-	// 65clean this shit up later, idk how classes work
-	std::string playertostring()
+	PlayerSide getplayer()
 	{
-		if (player::getplayer == 0)
+		return player;
+	}
+	std::string playertoString()
+	{
+		if (player == PlayerSide::X)
 		{
 			return "P1";
 		}
@@ -35,7 +47,7 @@ public:
 	}
 	char playertoChar()
 	{
-		if (player::getplayer == 0)
+		if (player == PlayerSide::X)
 		{
 			return 'X';
 		}
@@ -53,23 +65,24 @@ private:
 	uint32_t cols;
 	uint64_t numspaces;
 public:
-	// Default constructor for a 3x3 board
-	void board()
+	// Default constructor for a 3 x 3 board
+	tttBoard()
 	{
-		board = new char[9];
+		board = new char[9]; // I have been told that using malloc is bad
 		rows = 3;
 		cols = 3;
+		numspaces = 9;
 		for (uint8_t i = 0; i < 9; i++)
 		{
 			board[i] = 'U';
 		}
 	}
-	// Parametrized constructor for an mxn board
-	void board(uint32_t inrows, uint32_t incols)
+	// Parametrized constructor for an m x n board
+	tttBoard(uint32_t inrows, uint32_t incols)
 	{
 		rows = inrows;
 		cols = incols;
-		numspaces = rows * cols;
+		numspaces = (uint64_t)rows * (uint64_t)cols;
 		board = new char[numspaces];
 		for (uint64_t i = 0; i < numspaces; i++)
 		{
@@ -83,7 +96,7 @@ public:
 	}
 	// Place an 'X' or 'O' on the board
 	// returns TRUE if the marker was placed, FALSE otherwise
-	BOOL placeMarker(player inplayer, uint64_t pos)
+	BOOL placeMarker(tttPlayer inplayer, uint64_t pos)
 	{
 		assert(pos < numspaces);
 		if (board[pos] == 'U')
@@ -93,17 +106,114 @@ public:
 		}
 		return FALSE;
 	}
-	// checks if the end conditions of the game have been met
+	// checks if the winning/ ending conditions of the game have been met
 	// returns TRUE if so, FALSE otherwise
-	// winner arg returns with a 0 if P1 won, 1 if P2 won, and 255 if the game isn't over
-	BOOL checkendconditions(tttBoard* board, uint8_t* winner)
+	// winner arg returns with a X if P1 won, O if P2, and Cat if neither
+	BOOL CheckEndConditions(tttBoard* board, PlayerSide* winner)
 	{
-		// to be implemented...
+		BOOL indic;
+		BOOL gameover = TRUE;
+		char first;
+
+		for (uint64_t i = 0; i < numspaces; i++) // iterate through all spaces
+		{
+			if (board->board[i] == 'U')
+			{
+				gameover = FALSE;
+				break;
+			}
+		}
 
 		// check rows
+		for (uint32_t currRow = 0; currRow < rows; currRow++) // iterate through all rows
+		{
+			indic = TRUE;
+			first = board->board[currRow * cols]; // grab first entry from the current row
+			if (first != 'U') // avoid some uneccessary early game checks
+			{
+				for (uint32_t i = 1; i < cols; i++) // check each space in a given row
+				{
+					if (first != board->board[(currRow * cols) + i])
+					{
+						indic = FALSE;
+						break;
+					}
+				}
+				// check results
+				if (indic) 
+				{
+					goto WinnerReturn;
+				}
+			}
+		}
 
 		// check columns
-
+		for (uint32_t currCol = 0; currCol < cols; currCol++) // iterate through all rows
+		{
+			indic = TRUE;
+			first = board->board[currCol]; // grab first entry from the current column
+			if (first != 'U') // avoid some uneccessary early game checks
+			{
+				for (uint32_t i = 1; i < rows; i++) // check each space in a given column
+				{
+					if (first != board->board[(cols * i) + currCol])
+					{
+						indic = FALSE;
+						break;
+					}
+				}
+				// check results
+				if (indic)
+				{
+					goto WinnerReturn;
+				}
+			}
+		}
 		// check diagonals
+		if (rows == cols) // first need to make sure diagonals make sense on the given board
+		{
+			// top left to bottom right first
+			indic = TRUE;
+			first = board->board[0]; // grab first entry from the current row
+			for (uint32_t i = 1; i < cols; i++) // check each space in a given column
+			{
+				if (first != board->board[(cols * i) + i])
+				{
+					indic = FALSE;
+					break;
+				}
+			}
+			// check results
+			if (indic)
+			{
+				goto WinnerReturn;
+			}
+
+			// now top right to bottom left
+			// top left to bottom right first
+			indic = TRUE;
+			first = board->board[cols - 1]; // grab first entry from the current row
+			for (uint32_t i = 1; i < cols; i++) // check each space in a given column
+			{
+				if (first != board->board[(cols * i) + (cols - i - 1)])
+				{
+					indic = FALSE;
+					break;
+				}
+			}
+			// check results
+			if (indic)
+			{
+				goto WinnerReturn;
+			}
+		}
+
+		// no one has won 
+		*winner = PlayerSide::Cat; // games 'goes to the cat'
+		return gameover; 
+
+	WinnerReturn:
+		*winner = (first == 'X') ? PlayerSide::X : PlayerSide::O;
+		return TRUE;
 	}
 };
